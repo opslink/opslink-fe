b446cb7671288397bcf57f144d61ffa87bcb2fac
Duye <superdoye0120@outlook.com>
2023-12-12 10:25:32 +0800

    :fire: add some config
    - use antfu eslnt-config
    - use semi-design
    - use pnpm install

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import {Layout} from '@douyinfe/semi-ui';
+import React from 'react'
+import { Layout } from '@douyinfe/semi-ui'
 
-const { Header, Footer, Content } = Layout;
-function _Layout({children}:React.PropsWithChildren) {
+const { Header, Footer, Content } = Layout
+function _Layout({ children }: React.PropsWithChildren) {
   const commonStyle = {
     height: 64,
     lineHeight: '64px',
-    background: 'var(--semi-color-fill-0)'
-};
+    background: 'var(--semi-color-fill-0)',
+  }
   return (
-      <Layout className='w-full h-full'>
-        <Header style={commonStyle}>Header</Header>
-          <Content>{children}</Content>
-        <Footer style={commonStyle}>Footer</Footer>
-          </Layout>
-      
-  );
+    <Layout className="w-full h-full">
+      <Header style={commonStyle}>Header</Header>
+      <Content>{children}</Content>
+      <Footer style={commonStyle}>Footer</Footer>
+    </Layout>
+
+  )
 }
 
-export default _Layout;
+export default _Layout
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from 'next/app'
 import Layout from '@/components/layout'
 import './globals.css'
- 
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
